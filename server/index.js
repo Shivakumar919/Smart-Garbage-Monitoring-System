52086ca9980a930b753f2187926831e628971059
Shivakumar919 <shivakumar91999@gmail.com>
2026-07-07 22:11:29 +0530

    serve files

--- server/index.js
+++ server/index.js
@@ -1,1 +1,28 @@
 // main server file
+const express = require("express");
+
+const app = express();
+
+// Middleware
+app.use(express.json());
+
+// Home route
+app.get("/", (req, res) => {
+  res.send("Server is running...");
+});
+
+// Example API route
+app.get("/api/users", (req, res) => {
+  res.json({
+    name: "John",
+    age: 25
+  });
+});
+
+// Start server
+const PORT = 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
